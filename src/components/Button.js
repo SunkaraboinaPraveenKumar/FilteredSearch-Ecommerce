b954425e7fd859b4fb0a32802cf26c620361e588
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-05-02 19:54:47 +0530

    Filtering Advanced Ecommerce

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Button({onClickHandler,value,title}) {
+  return (
+    <button onClick={onClickHandler} value={value} className='btns'>{title}</button>
+  )
+}
+export default Button;
---
+++ src/components/Input.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Input({handleChange,value,title,name,color}) {
+  return (
+    <label className='sidebar-label-container'>
+    <input
+    onChange={handleChange}
+    value={value}
+
+    type="radio" 
+    name={name} />
+    <span className='checkmark' style={{backgroundColor:color}}></span>{title}
+  </label>
+  )
+}
